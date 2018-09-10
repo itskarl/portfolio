f1e1db5b0ce983e8c8c0892f70dac4ee2dc68b82
Karl Rodulfo <karlrodulfo@gmail.com>
2018-09-10 17:48:50 -0400

    initializing

---
+++ javascript.js
@@ -0,0 +1,24 @@
+console.log('lol')
+
+
+  $(function() {
+    $.scrollify({
+      section : ".profile-section",
+      sectionName : "section-name",
+      interstitialSection : "",
+      easing: "easeOutExpo",
+      scrollSpeed: 1100,
+      offset : 0,
+      scrollbars: true,
+      standardScrollElements: "",
+      setHeights: true,
+      overflowScroll: true,
+      updateHash: true,
+      touchScroll:true,
+      before:function() {},
+      after:function() {},
+      afterResize:function() {},
+      afterRender:function() {}
+    });
+     $(".links").on("click",$.scrollify.move);
+  });
